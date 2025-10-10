79101ea384e152afaea618b18e49a4d47b0839a2
jackveneri <120265777+jackveneri@users.noreply.github.com>
2025-10-10 21:10:44 +0200

    New Docs test

--- docs/make.jl
+++ docs/make.jl
@@ -1,17 +1,19 @@
-using Documenter, DocumenterVitepress
 using ESM_PINO
 include("../ext/ESM_PINOQG3Ext.jl")
 using .ESM_PINOQG3Ext
+using Documenter
+using DocumenterVitepress
+
 
 DocMeta.setdocmeta!(ESM_PINO, :DocTestSetup, :(using ESM_PINO); recursive=true)
 
 makedocs(;
     modules=[ESM_PINO, ESM_PINOQG3Ext],
+    warnonly = true,
     authors="Giacomo Veneri <giacomo.veneri@tum.de>",
     sitename="ESM_PINO.jl",
-    repo="https://github.com/jackveneri/ESM_PINO.jl.git",
-    format=DocumenterVitepress.MarkdownVitepress(repo = "github.com/jackveneri/ESM_PINO.jl.git", devbranch = "main",
-    devurl = "dev"),
+    repo="https://github.com/jackveneri/ESM_PINO.jl",
+    format=DocumenterVitepress.MarkdownVitepress(repo = "https://github.com/jackveneri/ESM_PINO.jl", devurl="dev", deploy_url="jackveneri.github.io/ESM_PINO.jl"),
     pages=[
         "Home" => "index.md",
         "Reference" => "ref.md",
@@ -19,8 +21,6 @@ makedocs(;
 )
 
 DocumenterVitepress.deploydocs(;
-    repo="github.com/jackveneri/ESM_PINO.jl.git",
-    target="build",
-    devbranch="main",
+    repo="github.com/jackveneri/ESM_PINO.jl",
     push_preview=true,
 )
